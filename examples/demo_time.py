9331866603d338ba19b37a9e461e33a55d3a376d
manas-shinde <manas.a.shinde11@gmail.com>
2025-06-03 11:39:47 +0530

    fix : update function name.

--- examples/demo_time.py
+++ examples/demo_time.py
@@ -2,10 +2,10 @@ from decorators.time_decorator import time_execution
 
 
 @time_execution
-def loop():
+def long_for_loop():
     for i in range(100000000):
         pass
 
 
 if __name__ == "__main__":
-    loop()
+    long_for_loop()
